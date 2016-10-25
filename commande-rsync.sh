afbb66bb27a5c7c658f205bd11d740d96d443fc6
Ben Lacherez <blacherez@gmail.com>
2016-10-25 22:49:30 +0200

    Updated rsync command to ignore work folder, .git and rsync script itself.

--- commande-rsync.sh
+++ commande-rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --exclude .git . pvbx:/kunden/homepages/34/d532655029/htdocs/lacherez.info/landing
+rsync -avz --exclude-from .exclude . pvbx:/kunden/homepages/34/d532655029/htdocs/lacherez.info/landing
